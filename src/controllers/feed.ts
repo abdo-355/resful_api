1e89baae8622511af0aa684cf125320b878a5915
abdo-355 <abdoluxor366@gmail.com>
2022-11-18 21:14:35 +0200

    use the post model to save posts to the database in the createPost controller

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
 
+import Post from "../models/post";
+
 export const getPosts: RequestHandler = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -18,30 +20,34 @@ export const getPosts: RequestHandler = (req, res, next) => {
   });
 };
 
-export const createPost: RequestHandler = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
+export const createPost: RequestHandler = async (req, res, next) => {
+  try {
+    const title = req.body.title;
+    const content = req.body.content;
 
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
         message: "Validation failed, entered data is incorrect",
         errors: errors.array(),
       });
-  }
+    }
 
-  // TODO: Create post in the db
-  res.status(201).json({
-    message: "Post saved successfully",
-    post: {
-      id: new Date().toISOString(),
+    const post = new Post({
       title,
       content,
+      imgUrl: "this is an image url",
       creator: { name: "Abdo" },
-      createdAt: new Date(),
-    },
-  });
+    });
+
+    const result = await post.save();
+
+    res.status(201).json({
+      message: "Post saved successfully",
+      post: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
