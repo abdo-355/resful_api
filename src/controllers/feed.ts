c7cb8f273baa6cd4360943a885f1e7a76b903175
abdo-355 <abdoluxor366@gmail.com>
2022-11-23 15:15:00 +0200

    add editing a post functionality

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
+import fs from "fs";
+import path from "path";
 
 import Post from "../models/post";
 
@@ -62,8 +64,6 @@ export const createPost: RequestHandler = async (req, res, next) => {
     }
 
     const imgUrl = req.file.path.replace("\\", "/");
-    console.log(req.file.path);
-    console.log(imgUrl);
 
     const post = new Post({
       title,
@@ -84,4 +84,65 @@ export const createPost: RequestHandler = async (req, res, next) => {
     }
     next(err);
   }
+};
+
+export const updatePost: RequestHandler = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error: ResponseError = new Error(
+        "Validation failed, entered data is incorrect"
+      );
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const title = req.body.title;
+    const content = req.body.content;
+    let imgUrl = req.body.image;
+
+    if (req.file) {
+      imgUrl = req.file.path.replace("\\", "/");
+    }
+
+    if (!imgUrl) {
+      const error: ResponseError = new Error("No file picked");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error: ResponseError = new Error("could not find post");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (imgUrl !== post.imgUrl) {
+      clearImage(post.imgUrl);
+    }
+
+    post.title = title;
+    post.content = content;
+    post.imgUrl = imgUrl;
+    const result = await post.save();
+
+    res
+      .status(200)
+      .json({ message: "Post updated successfully", post: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+const clearImage = (filePath: string) => {
+  filePath = path.join(__dirname, "..", "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
