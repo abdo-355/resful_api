7089054d983d699309b314309862515ffe4a60bb
abdo-355 <abdoluxor366@gmail.com>
2022-12-15 21:19:23 +0200

    test the getStatus controller successfully

--- src/tests/auth-controllers.spec.ts
+++ src/tests/auth-controllers.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 import User from "../models/user";
 import * as authController from "../controllers/auth";
@@ -31,5 +32,54 @@ describe("auth controllers - login", () => {
     expect(data).to.have.property("statusCode", 500);
 
     (User.findOne as sinon.SinonStub).restore();
+  });
+});
+
+describe("auth controllers - status", () => {
+  it("should send a response with a valid user status for an existing user", async () => {
+    // for deprication error
+    mongoose.set("strictQuery", true);
+    // ------------------------
+    await mongoose.connect(process.env.MONGO_TESTING_URI!);
+
+    const user = new User({
+      _id: "5c0f66b979af55031b34728a",
+      name: "tester",
+      email: "tester@testing.com",
+      password: "1235testing",
+    });
+
+    await user.save();
+
+    const req = {
+      userId: "5c0f66b979af55031b34728a",
+    } as Request;
+
+    const res = {
+      statusCode: 500,
+      userStatus: "",
+      status: function (status: number) {
+        this.statusCode = status;
+        // in order to call json() after status
+        return this;
+      },
+      json: function (data: { status: string }) {
+        this.userStatus = data.status;
+      },
+    } as any;
+
+    await (authController.getUserStatus(
+      req,
+      res,
+      () => {}
+    ) as unknown as Promise<any>);
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.userStatus).to.equal("I am new!");
+
+    // delete the created user and close th db connection after finishing the test
+    await User.deleteMany({});
+    await mongoose.disconnect();
+    //---------------------------------------------------
   });
 });
